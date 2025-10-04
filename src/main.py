47d5d65c214cab55c0b498c55d56ae5cbfe2f39d
Шамаева Елена Денисовна <eshamaeva@stsoft.ru>
2025-10-04 23:44:57 +0300

    Adding parameters with several values

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import argparse
+import copy
 import random
 import os
 import yaml
 
 from deppllama_train_qlora import conduct_experiment
-from parameters import Parameters
+from parameters import Parameters, SeveralParameters
 
 random.seed(23)
 
@@ -18,12 +19,25 @@ with open(config_name, 'r') as file:
 
 print(configs)
 
-parameters = Parameters(configs)
+main_parameters = Parameters(configs)
+several_parameters = SeveralParameters(configs) # TODO: rename
 
-os.makedirs(parameters.output_dir_path)
-with open(parameters.output_dir_path + config_name.split('/')[-1], 'w') as file:
+os.makedirs(main_parameters.output_dir_path)
+with open(main_parameters.output_dir_path + config_name.split('/')[-1], 'w') as file:
     yaml.dump(configs, file, default_flow_style=False)
 
-print("LEARNING_RATE:\t" + str(parameters.learning_rate))
+print("LEARNING_RATE:\t" + str(main_parameters.learning_rate))
 
-conduct_experiment(parameters)
+experiment_number = 0
+for lora_alpha in several_parameters.lora_alpha: # TODO: автоматический перебор
+    for lora_r in several_parameters.lora_r:
+        for lora_dropout in several_parameters.lora_dropout:
+            cur_parameters = copy.deepcopy(main_parameters)
+            cur_parameters.output_dir_path = f"{cur_parameters.output_dir_path}/{experiment_number}"
+            cur_parameters.lora_alpha = lora_alpha
+            cur_parameters.lora_r = lora_r
+            cur_parameters.lora_dropout = lora_dropout
+
+            os.makedirs(cur_parameters.output_dir_path)
+            conduct_experiment(cur_parameters)
+            experiment_number += 1

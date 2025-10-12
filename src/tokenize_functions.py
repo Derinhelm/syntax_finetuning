0f88f999db7d84d428b475244fca625f9140f19b
Шамаева Елена Денисовна <eshamaeva@stsoft.ru>
2025-10-12 18:00:47 +0300

    Deleting unused add_eos_token

--- src/tokenize_functions.py
+++ src/tokenize_functions.py
@@ -8,7 +8,7 @@ class Tokenizer:
             parameters.model_config.model_name, trust_remote_code=True)
 
     # Notice: in the generate_and_tokenize_prompt function result["labels"] is rewritten
-    def tokenize_base(self, prompt, cutoff_len, add_eos_token=True):
+    def tokenize_base(self, prompt, cutoff_len):
         result = self.tokenizer(
             prompt,
             truncation=True,
@@ -19,7 +19,6 @@ class Tokenizer:
         if (
             result["input_ids"][-1] != self.tokenizer.eos_token_id
             and len(result["input_ids"]) < cutoff_len
-            and add_eos_token
         ):
             result["input_ids"].append(self.tokenizer.eos_token_id)
             result["attention_mask"].append(1)
@@ -29,11 +28,11 @@ class Tokenizer:
         return result
 
     # Notice: in the generate_and_tokenize_prompt function result["labels"] is rewritten
-    def tokenize(self, prompt, cutoff_len, add_eos_token=True):
-        return self.tokenize_base(prompt, cutoff_len, add_eos_token)
+    def tokenize(self, prompt, cutoff_len):
+        return self.tokenize_base(prompt, cutoff_len)
 
     # Notice: result["labels"] is rewritten so that only the output is considered
-    def generate_and_tokenize_prompt(self, data_point, add_eos_token=True):
+    def generate_and_tokenize_prompt(self, data_point):
         full_prompt = generate_prompt(data_point)
         tokenized_full_prompt = self.tokenize(full_prompt, CUTOFF_LEN)
 
@@ -41,12 +40,11 @@ class Tokenizer:
             data_point["input"]
         )
         tokenized_user_prompt = self.tokenize(
-            user_prompt, CUTOFF_LEN, add_eos_token=add_eos_token
+            user_prompt, CUTOFF_LEN, add_eos_token=True
         )
         user_prompt_len = len(tokenized_user_prompt["input_ids"])
 
-        if add_eos_token:
-            user_prompt_len -= 1
+        user_prompt_len -= 1
 
         tokenized_full_prompt["labels"] = [
             -100

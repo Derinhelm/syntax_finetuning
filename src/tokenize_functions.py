2c4c9076b9e9d4f82234d4c5832637bf2cbdbe61
Шамаева Елена Денисовна <Derinhelm@yandex.ru>
2025-10-12 18:02:30 +0300

    Deleting unused CUTOFF_LEN in tokenize

--- src/tokenize_functions.py
+++ src/tokenize_functions.py
@@ -1,5 +1,4 @@
 from transformers import AutoTokenizer
-from constants import CUTOFF_LEN
 from deppllama_utils import *
 
 class Tokenizer:
@@ -8,49 +7,30 @@ class Tokenizer:
             parameters.model_config.model_name, trust_remote_code=True)
 
     # Notice: in the generate_and_tokenize_prompt function result["labels"] is rewritten
-    def tokenize_base(self, prompt, cutoff_len):
-        result = self.tokenizer(
-            prompt,
-            truncation=True,
-            max_length=cutoff_len,
-            padding=False,
-            return_tensors=None,
-        )
-        if (
-            result["input_ids"][-1] != self.tokenizer.eos_token_id
-            and len(result["input_ids"]) < cutoff_len
-        ):
+    def tokenize_base(self, prompt):
+        result = self.tokenizer(prompt, return_tensors=None)
+        if (result["input_ids"][-1] != self.tokenizer.eos_token_id):
             result["input_ids"].append(self.tokenizer.eos_token_id)
             result["attention_mask"].append(1)
     
         result["labels"] = result["input_ids"].copy()
-    
         return result
 
     # Notice: in the generate_and_tokenize_prompt function result["labels"] is rewritten
-    def tokenize(self, prompt, cutoff_len):
-        return self.tokenize_base(prompt, cutoff_len)
+    def tokenize(self, prompt):
+        return self.tokenize_base(prompt)
 
     # Notice: result["labels"] is rewritten so that only the output is considered
     def generate_and_tokenize_prompt(self, data_point):
         full_prompt = generate_prompt(data_point)
-        tokenized_full_prompt = self.tokenize(full_prompt, CUTOFF_LEN)
+        tokenized_full_prompt = self.tokenize(full_prompt)
 
-        user_prompt = generate_prompt_str(
-            data_point["input"]
-        )
-        tokenized_user_prompt = self.tokenize(
-            user_prompt, CUTOFF_LEN, add_eos_token=True
-        )
-        user_prompt_len = len(tokenized_user_prompt["input_ids"])
+        user_prompt = generate_prompt_str(data_point["input"])
+        tokenized_user_prompt = self.tokenize(user_prompt, add_eos_token=True)
+        user_prompt_len = len(tokenized_user_prompt["input_ids"]) - 1 # Minus eos-token
 
-        user_prompt_len -= 1
-
-        tokenized_full_prompt["labels"] = [
-            -100
-        ] * user_prompt_len + tokenized_full_prompt["labels"][
-            user_prompt_len:
-        ]  # could be sped up, probably
+        tokenized_full_prompt["labels"] = [-100] * user_prompt_len + \
+            tokenized_full_prompt["labels"][user_prompt_len:]  # could be sped up, probably
         return tokenized_full_prompt
 
     def set_tokens(self, parameters):

94f42ccd9e445297aab54fd945d7af653d0fc922
Шамаева Елена Денисовна <Derinhelm@yandex.ru>
2025-10-18 21:58:59 +0300

    Deleting lm_head from target modules

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,5 @@
-LORA_TARGET_MODULES = ['q_proj','k_proj','v_proj','o_proj','gate_proj','down_proj','up_proj','lm_head']
+#LORA_TARGET_MODULES = ['q_proj','k_proj','v_proj','o_proj','gate_proj','down_proj','up_proj','lm_head']
+LORA_TARGET_MODULES = ['q_proj','k_proj','v_proj','o_proj','gate_proj','down_proj','up_proj']
 
 CUTOFF_LEN = 512
 

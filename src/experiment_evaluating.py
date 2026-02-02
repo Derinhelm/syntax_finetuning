64749667f84b34508a23fb67f55248bd6dedbee0
Шамаева Елена Денисовна <Derinhelm@yandex.ru>
2026-02-02 12:55:25 +0300

    Fixing bugs with metrics

--- src/experiment_evaluating.py
+++ src/experiment_evaluating.py
@@ -1,5 +1,5 @@
-from src.metric_functions.metric_calculating import calculate_metrics
-from src.metric_functions.category_calculating import create_statistics
+from metric_functions.metric_calculating import calculate_metrics
+from metric_functions.category_calculating import create_statistics
     
 import argparse
 import os
@@ -15,11 +15,19 @@ def print_mean_metrics(uas_metrics, las_metrics):
     good_las = [r for r in las_metrics if r is not None]
     bad_las = [r for r in las_metrics if r is None]
     if good_uas:
-        print(f"{sum(good_uas) / len(good_uas) * 100:.2f}% ({sum(good_uas) / len(uas_metrics) * 100:.2f}%)")
+        print(f"{sum(good_uas) / len(good_uas) * 100:.1f}% ({sum(good_uas) / len(uas_metrics) * 100:.1f}%)")
     if good_las:
-        print(f"{sum(good_las) / len(good_las) * 100:.2f}% ({sum(good_las) / len(las_metrics) * 100:.2f}%)")
+        print(f"{sum(good_las) / len(good_las) * 100:.1f}% ({sum(good_las) / len(las_metrics) * 100:.1f}%)")
     print(len(bad_uas), len(uas_metrics))#len(bad_las))
     
+def process_sentence(gold_tree, parser_tree):
+    output_r, output_errors, output_extra_pred_tokens = create_statistics(
+        gold_tree, parser_tree)
+    # TODO: output_errors и output_extra_pred_tokens - для сохранения
+    sent_uas, sent_las = \
+        calculate_metrics(output_r)
+    return sent_uas, sent_las
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Getting config name')
     parser.add_argument('-c','--config', help='Config Name',
@@ -29,6 +37,8 @@ if __name__ == "__main__":
         config = yaml.safe_load(file)
         print(config)
     gold_path = config["gold_file"]
+    
+    format = config.get("format", "jsonl")
 
     pred_filenames = []
     for file_directory in config['pred_directories']:
@@ -42,44 +52,33 @@ if __name__ == "__main__":
     sentences = parse(content)
     
     
-    config_results = {}
     config_uas, config_las = {}, {}
-    config_errors = {}
-    config_extra_pred_tokens = {}
     for pred_filename in pred_filenames:
         print(pred_filename)
         try:
-          pred_trees = []
-          with open(filepath, 'r', encoding='utf-8') as f:
+          if format == "jsonl":
+            pred_trees = []
+            with open(pred_filename, 'r', encoding='utf-8') as f:
               for line_num, line in enumerate(f, 1):         
                   item = json.loads(line)
                   pred_trees.append(item)
+          else:
+            with open(pred_filename, 'r', encoding='utf-8') as f:
+                pred_trees = json.load(f)
 
           assert len(sentences) == len(pred_trees)
-          config_results[pred_filename] = []
-          config_errors[pred_filename] = []
-          config_extra_pred_tokens[pred_filename] = []
-          for sent_i in range(len(sentences)):
-            try:
-                output_r, output_errors, output_extra_pred_tokens = create_statistics(sentences[sent_i], pred_trees[sent_i])
-                config_results[pred_filename].append(output_r)
-                config_extra_pred_tokens[pred_filename].append(output_extra_pred_tokens)
-                if output_errors:
-                    config_errors[pred_filename].append(output_errors)
-            except Exception:
-                config_results[pred_filename].append(None)
-                # TODO (для предложений из нескольких предложений)
-
           config_uas[pred_filename], config_las[pred_filename] = [], []
-          for sent_r in config_results[pred_filename]:
-            if r is None: # TODO
+
+          for sent_i, sent_r in enumerate(sentences):
+            try:
+                sent_uas, sent_las = process_sentence(sentences[sent_i], pred_trees[sent_i])
+                config_uas[pred_filename].append(sent_uas)
+                config_las[pred_filename].append(sent_las)
+            except Exception:
+                # TODO (для предложений из нескольких предложений)
                 config_uas[pred_filename].append(1)
                 config_las[pred_filename].append(1)
-            else:   
-              sent_uas, sent_las = \
-                calculate_metrics(sent_r)
-              config_uas[pred_filename].append(uas)
-              config_las[pred_filename].append(las)
+
           del pred_trees
           gc.collect()
         except Exception as e:

819eef4b82ec51f8a0666938b915bb4438c01ed0
derinhelm <derinhelm@yandex.ru>
2026-03-13 10:40:44 +0300

    Fixing cpu parameter

--- src/inference_parser.py
+++ src/inference_parser.py
@@ -141,7 +141,7 @@ def start_parallel_inference_experiment(exp_list, process_i, parallel_path, star
 
     memory_limit = 20 * 1024 * 1024 * 1024  # Max - 25 GB
     resource.setrlimit(resource.RLIMIT_AS, (memory_limit, memory_limit))
-    cpus = set(range(process_i * 128, (process_i + 1) * 128))
+    cpus = set(range(process_i * 16, (process_i + 1) * 16))
     os.sched_setaffinity(0, cpus)
     os.environ["CUDA_VISIBLE_DEVICES"] = str(process_i)
     time.sleep(10)

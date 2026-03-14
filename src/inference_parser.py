7ab8860413366d25e22fcfbad73e641e7f607c1d
derinhelm <derinhelm@yandex.ru>
2026-03-14 15:43:45 +0300

    Changing memory_limit

--- src/inference_parser.py
+++ src/inference_parser.py
@@ -139,7 +139,7 @@ def start_parallel_inference_experiment(exp_list, process_i, parallel_path, star
     sys.stderr = stderr_file
 
 
-    memory_limit = 20 * 1024 * 1024 * 1024  # Max - 25 GB
+    memory_limit = 150 * 1024 * 1024 * 1024
     resource.setrlimit(resource.RLIMIT_AS, (memory_limit, memory_limit))
     cpus = set(range(process_i * 16, (process_i + 1) * 16))
     os.sched_setaffinity(0, cpus)

6c44feb9b03c87c51924e7aa99fb69e615b219d9
derinhelm <derinhelm@yandex.ru>
2026-04-13 20:48:49 +0300

    Extracting run_all_experiments (for inference)

--- src/inference.py
+++ src/inference.py
@@ -10,6 +10,45 @@ import yaml
 from inference_parser import start_inference_experiment, \
     start_parallel_inference_experiment, create_adapter_name
 
+def run_all_experiments(parallel_config, experiments):
+    if not parallel_config:
+        for exp in experiments:
+            start_inference_experiment(exp)
+    else:
+            process_num = 8
+            exp_groups = [[] for _ in range(process_num)]
+            for i, item in enumerate(experiments):
+                exp_groups[i % process_num].append(item)
+
+            mp.set_start_method('spawn', force=True)
+             
+            start_time = datetime.now().strftime("%D %H:%M:%S").replace("/", "_").replace(":", "_").replace(" ", "_")
+            processes = []
+            parallel_path = parallel_config["parallel_path"]
+            for i in range(process_num):
+                p = mp.Process(target=start_parallel_inference_experiment,
+                    args=(exp_groups[i], i, parallel_path, start_time))
+                processes.append(p)
+                p.start()
+                print(f"Process {i}. is_alive: {p.is_alive()}, params: {p.__dict__}")
+                time.sleep(200)
+            
+            while any(p.is_alive() or p.exitcode != 0 for p in processes):
+                for i, p in enumerate(processes):
+                    if not p.is_alive() and p.exitcode != 0:
+                        cur_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        print(f"{cur_time}. Error {i} process: {p.exitcode}")
+                        processes[i] = mp.Process(target=start_parallel_inference_experiment,
+                            args=(exp_groups[i], i, parallel_path, start_time))
+                        processes[i].start()
+                        cur_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        print(f"{cur_time}. Process {i} is restarted")
+                        time.sleep(10)
+                        print(f"Process {i}. is_alive: {p.is_alive()}, params: {p.__dict__}")
+
+                time.sleep(30)
+
+
 def inference_main():
     os.environ["VLLM_ENGINE_ITERATION_TIMEOUT_S"] = "300"
     parser = argparse.ArgumentParser()
@@ -72,42 +111,7 @@ def inference_main():
             print(experiments[-1])
         print(len(experiments))
 
-    if not parallel_config:
-        for exp in experiments:
-            start_inference_experiment(exp)
-    else:
-            process_num = 8
-            exp_groups = [[] for _ in range(process_num)]
-            for i, item in enumerate(experiments):
-                exp_groups[i % process_num].append(item)
-
-            mp.set_start_method('spawn', force=True)
-             
-            start_time = datetime.now().strftime("%D %H:%M:%S").replace("/", "_").replace(":", "_").replace(" ", "_")
-            processes = []
-            parallel_path = parallel_config["parallel_path"]
-            for i in range(process_num):
-                p = mp.Process(target=start_parallel_inference_experiment,
-                    args=(exp_groups[i], i, parallel_path, start_time))
-                processes.append(p)
-                p.start()
-                print(f"Process {i}. is_alive: {p.is_alive()}, params: {p.__dict__}")
-                time.sleep(200)
-            
-            while any(p.is_alive() or p.exitcode != 0 for p in processes):
-                for i, p in enumerate(processes):
-                    if not p.is_alive() and p.exitcode != 0:
-                        cur_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                        print(f"{cur_time}. Error {i} process: {p.exitcode}")
-                        processes[i] = mp.Process(target=start_parallel_inference_experiment,
-                            args=(exp_groups[i], i, parallel_path, start_time))
-                        processes[i].start()
-                        cur_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                        print(f"{cur_time}. Process {i} is restarted")
-                        time.sleep(10)
-                        print(f"Process {i}. is_alive: {p.is_alive()}, params: {p.__dict__}")
-
-                time.sleep(30)
+    run_all_experiments(parallel_config, experiments)
 
 if __name__ == "__main__":
     inference_main()

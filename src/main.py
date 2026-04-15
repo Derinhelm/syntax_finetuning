b5e8021d3109a3e3c7ccb36eedd40aca61bc4f72
derinhelm <derinhelm@yandex.ru>
2026-04-15 11:15:38 +0300

    Creating finetuning_main

--- src/main.py
+++ src/main.py
@@ -11,77 +11,81 @@ from parameters import Parameters
 from single_function_executor import FineTuningExecutor
 from start_experiments import run_all_experiments
 
-parser = argparse.ArgumentParser()
-parser.add_argument("config_name", nargs='?', default='/src/src/configs/config.yaml')
-parser_args = parser.parse_args()
-config_name = parser_args.config_name
+def finetuning_main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_name", nargs='?', default='/src/src/configs/config.yaml')
+    parser_args = parser.parse_args()
+    config_name = parser_args.config_name
 
-with open(config_name, 'r') as file:
-    configs = yaml.safe_load(file)
+    with open(config_name, 'r') as file:
+        configs = yaml.safe_load(file)
 
 
-parallel_config = {}
-if "parallel_config" in configs:
-    parallel_config = configs.pop("parallel_config")
+    parallel_config = {}
+    if "parallel_config" in configs:
+        parallel_config = configs.pop("parallel_config")
 
-parameters = Parameters(config_name)
-dataset_configs, model_configs = [], []
-several_parameters = OrderedDict()
-for param_name, param_values in configs.items():
-    if param_name == 'dataset_config':
-        if isinstance(configs['dataset_config'], list):
-            dataset_configs = [ DatasetConfig(path_c) for path_c in configs['dataset_config'] ]
+    parameters = Parameters(config_name)
+    dataset_configs, model_configs = [], []
+    several_parameters = OrderedDict()
+    for param_name, param_values in configs.items():
+        if param_name == 'dataset_config':
+            if isinstance(configs['dataset_config'], list):
+                dataset_configs = [ DatasetConfig(path_c) for path_c in configs['dataset_config'] ]
+            else:
+                dataset_configs = [ DatasetConfig(configs['dataset_config']) ]
+        elif param_name == 'model_config':
+            if isinstance(configs['model_config'], list):
+                model_configs = [ ModelConfig(path_c) for path_c in configs['model_config'] ]
+            else:
+                model_configs = [ ModelConfig(configs['model_config']) ]
         else:
-            dataset_configs = [ DatasetConfig(configs['dataset_config']) ]
-    elif param_name == 'model_config':
-        if isinstance(configs['model_config'], list):
-            model_configs = [ ModelConfig(path_c) for path_c in configs['model_config'] ]
-        else:
-            model_configs = [ ModelConfig(configs['model_config']) ]
-    else:
-        if isinstance(param_values, list):
-            several_parameters[param_name] = param_values # Several parameters
-        else:
-            parameters.__setattr__(param_name, param_values) # One parameter
+            if isinstance(param_values, list):
+                several_parameters[param_name] = param_values # Several parameters
+            else:
+                parameters.__setattr__(param_name, param_values) # One parameter
 
-print(dataset_configs)
-print(model_configs)
-several_param_names = list(several_parameters.keys())
-s_params = list(itertools.product(*several_parameters.values()))
-if not s_params:
-    s_params = [{}]
+    print(dataset_configs)
+    print(model_configs)
+    several_param_names = list(several_parameters.keys())
+    s_params = list(itertools.product(*several_parameters.values()))
+    if not s_params:
+        s_params = [{}]
 
-experiments = []
-for model_i, model_config in enumerate(model_configs):
-    for dataset_i, dataset_config in enumerate(dataset_configs):
-        parameters.model_config = model_config 
-        parameters.dataset_config = dataset_config
-        os.makedirs(parameters.output_model_dataset_path)
+    experiments = []
+    for model_i, model_config in enumerate(model_configs):
+        for dataset_i, dataset_config in enumerate(dataset_configs):
+            parameters.model_config = model_config 
+            parameters.dataset_config = dataset_config
+            os.makedirs(parameters.output_model_dataset_path)
 
-        config_dir_path = parameters.config_dir_path
-        with open(config_dir_path, 'w') as file:
-            yaml.dump(configs, file, default_flow_style=False)
+            config_dir_path = parameters.config_dir_path
+            with open(config_dir_path, 'w') as file:
+                yaml.dump(configs, file, default_flow_style=False)
 
-        for experiment_number, experiment_params in enumerate(s_params):
-            #print(f"Model {model_i} from {len(model_configs)}")
-            #print(f"Dataset {dataset_i} from {len(dataset_configs)}")
-            #print(f"Experiment {experiment_number} from {len(s_params)}")
-            #print(experiment_params)
-            #print(several_param_names)
-            assert len(experiment_params) == len(several_param_names)
-            cur_parameters = copy.deepcopy(parameters)
-            for param_i, param in enumerate(experiment_params):
-                cur_parameters.__setattr__(several_param_names[param_i], param)
-            cur_parameters.experiment_number = experiment_number
-            #print("-" * 10, cur_parameters.__dict__, sep='\n')
+            for experiment_number, experiment_params in enumerate(s_params):
+                #print(f"Model {model_i} from {len(model_configs)}")
+                #print(f"Dataset {dataset_i} from {len(dataset_configs)}")
+                #print(f"Experiment {experiment_number} from {len(s_params)}")
+                #print(experiment_params)
+                #print(several_param_names)
+                assert len(experiment_params) == len(several_param_names)
+                cur_parameters = copy.deepcopy(parameters)
+                for param_i, param in enumerate(experiment_params):
+                    cur_parameters.__setattr__(several_param_names[param_i], param)
+                cur_parameters.experiment_number = experiment_number
+                #print("-" * 10, cur_parameters.__dict__, sep='\n')
 
-            os.makedirs(cur_parameters.output_experiment_path)
-            experiments.append(cur_parameters)
+                os.makedirs(cur_parameters.output_experiment_path)
+                experiments.append(cur_parameters)
 
-print(f"Experiment amount: {len(experiments)}")
+    print(f"Experiment amount: {len(experiments)}")
 
-function_executor = FineTuningExecutor() # TODO: сделать выбор
-run_all_experiments(parallel_config, experiments,
-        function_executor)
+    function_executor = FineTuningExecutor() # TODO: сделать выбор
+    run_all_experiments(parallel_config, experiments,
+            function_executor)
 
-print("Finish")
+    print("Finish")
+
+if __name__ == "__main__":
+    finetuning_main()

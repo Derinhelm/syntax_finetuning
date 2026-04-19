ebd23416e206a7bade1f027764a9bc981648e965
derinhelm <derinhelm@yandex.ru>
2026-04-19 13:55:39 +0300

    Extracting code to evaluate_one_experiment

--- src/experiment_evaluating.py
+++ src/experiment_evaluating.py
@@ -5,24 +5,8 @@ import yaml
 
 from conllu import parse
 import json
-import gc
 
-from metric_functions.category_calculating \
-            import create_statistics
-from metric_functions.data_preparing import preprocess_tree
-
-
-def get_pred_trees(pred_filename, format):
-    if format == "jsonl":
-        pred_trees = []
-        with open(pred_filename, 'r', encoding='utf-8') as f:
-            for line_num, line in enumerate(f, 1):         
-                item = json.loads(line)
-                pred_trees.append(item)
-    else:
-        with open(pred_filename, 'r', encoding='utf-8') as f:
-            pred_trees = json.load(f)
-    return pred_trees
+from metric_functions.evaluate_one import evaluate_one_experiment
 
 def print_mean_metrics(uas_metrics, las_metrics):
     good_uas = [r for r in uas_metrics if r is not None]
@@ -50,55 +34,20 @@ if __name__ == "__main__":
     pred_filenames = []
     for file_directory in config['pred_directories']:
         new_pred_filenames = os.listdir(file_directory)
-        new_pred_filenames = [ f"{file_directory}/{filename}" for filename in new_pred_filenames if filename[0] != "." ]
+        new_pred_filenames = [ f"{file_directory}/{filename}"
+            for filename in new_pred_filenames if filename[0] != "." ]
         pred_filenames += new_pred_filenames
-
     
     with open(gold_path, 'r') as file:
         content = file.read()
-    sentences = parse(content)
-    
+    gold_sentences = parse(content)
     
     results = {}
     for pred_filename in sorted(pred_filenames):
         print(pred_filename)
         try:
-            pred_trees = get_pred_trees(pred_filename, format)
-            if len(sentences) != len(pred_trees):
-                print(f"Gold sents: {len(sentences)}, pred sents: {len(pred_trees)}")
-                gold_sent_ids = set(range(len(sentences)))
-                pred_sent_ids = {s['index'] for s in pred_trees}
-                #print(list(gold_sent_ids)[:10])
-                #print(list(pred_sent_ids)[:10])
-                print(f"Extra: {sorted(list(pred_sent_ids - gold_sent_ids))}")
-                print(f"Lost: {sorted(list(gold_sent_ids - pred_sent_ids))}")
-
-            assert len(sentences) == len(pred_trees)
-            expir_res_uas, expir_res_las = [], []
-            pred_trees_dict = {tree['index']:tree for tree in pred_trees}
-            assert len(pred_trees_dict) == len(pred_trees)
-            for sent_i, sent_r in enumerate(sentences):
-              try:
-                if isinstance(pred_trees[sent_i]["pred_tree"], list):
-                    gold_tree = [{'id': str(t['id']), 'form': t['form'],
-                        'parent_id': str(t['head']), 'relation': t['deprel'],
-                        'pos': t['upos'], 'feats': t['feats']} for t in sentences[sent_i]]
-                    gold_text = sentences[sent_i].metadata['text']
-                    gold_tree = preprocess_tree(gold_tree)
-                    pred_tree = preprocess_tree(pred_trees_dict[sent_i]["pred_tree"])
-                    		#pred_trees[sent_i]["pred_tree"])
-                    sent_uas, sent_las = create_statistics(gold_text, gold_tree,
-                        pred_tree, metric_type)
-                else: # Предложение с некорректным результатом
-                    sent_uas, sent_las = None, None
-                expir_res_uas.append(sent_uas)
-                expir_res_las.append(sent_las)
-              except Exception as e:
-                print(sent_i, e)
-
-            del pred_trees
-            gc.collect()
-
+            expir_res_uas, expir_res_las = evaluate_one_experiment(
+                gold_sentences, pred_filename, metric_type)
             print_mean_metrics(expir_res_uas, expir_res_las)
             short_filename = pred_filename.split("/")[-1].split(".")[0]
             results[f"{short_filename}_uas"] = expir_res_uas
@@ -110,4 +59,3 @@ if __name__ == "__main__":
 
     with open(result_path, 'w') as f:
         json.dump(results, f, indent=4) # Using indent for pretty-printing
-
---
+++ src/metric_functions/evaluate_one.py
@@ -0,0 +1,59 @@
+import json
+import gc
+
+from metric_functions.category_calculating \
+            import create_statistics
+from metric_functions.data_preparing import preprocess_tree
+
+
+def get_pred_trees(pred_filename, format):
+    if format == "jsonl":
+        pred_trees = []
+        with open(pred_filename, 'r', encoding='utf-8') as f:
+            for line_num, line in enumerate(f, 1):         
+                item = json.loads(line)
+                pred_trees.append(item)
+    else:
+        with open(pred_filename, 'r', encoding='utf-8') as f:
+            pred_trees = json.load(f)
+    return pred_trees
+
+def evaluate_one_experiment(gold_sentences, pred_filename, metric_type):
+    pred_trees = get_pred_trees(pred_filename, format)
+    if len(gold_sentences) != len(pred_trees):
+        print(f"Gold sents: {len(gold_sentences)}, pred sents: {len(pred_trees)}")
+        gold_sent_ids = set(range(len(gold_sentences)))
+        pred_sent_ids = {s['index'] for s in pred_trees}
+        #print(list(gold_sent_ids)[:10])
+        #print(list(pred_sent_ids)[:10])
+        print(f"Extra: {sorted(list(pred_sent_ids - gold_sent_ids))}")
+        print(f"Lost: {sorted(list(gold_sent_ids - pred_sent_ids))}")
+
+    assert len(gold_sentences) == len(pred_trees)
+
+    expir_res_uas, expir_res_las = [], []
+    pred_trees_dict = {tree['index']:tree for tree in pred_trees}
+    assert len(pred_trees_dict) == len(pred_trees)
+    for sent_i, sent_r in enumerate(gold_sentences):
+        try:
+            if isinstance(pred_trees[sent_i]["pred_tree"], list):
+                gold_tree = [{'id': str(t['id']), 'form': t['form'],
+                    'parent_id': str(t['head']), 'relation': t['deprel'],
+                    'pos': t['upos'], 'feats': t['feats']}
+                        for t in gold_sentences[sent_i]]
+                gold_text = gold_sentences[sent_i].metadata['text']
+                gold_tree = preprocess_tree(gold_tree)
+                pred_tree = preprocess_tree(
+                    pred_trees_dict[sent_i]["pred_tree"])
+                sent_uas, sent_las = create_statistics(gold_text, gold_tree,
+                    pred_tree, metric_type)
+            else: # Предложение с некорректным результатом
+                sent_uas, sent_las = None, None
+            expir_res_uas.append(sent_uas)
+            expir_res_las.append(sent_las)
+        except Exception as e:
+            print(sent_i, e)
+
+    del pred_trees
+    gc.collect()
+    return expir_res_uas, expir_res_las

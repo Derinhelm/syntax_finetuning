0c28428575811d3207c2e55fb63a322c4831b7d8
derinhelm <derinhelm@yandex.ru>
2026-04-26 14:59:31 +0300

    Creating create_finetuning_experiments

---
+++ src/finetuning_parsing.py
@@ -0,0 +1,45 @@
+import copy
+import os
+import yaml
+
+from parameters import Parameters
+
+from config_parsing import parse_field, get_several_config_params
+from config import DatasetConfig, ModelConfig
+
+
+def create_finetuning_experiments(configs, config_name):
+    dataset_configs = parse_field(configs, "dataset_config", DatasetConfig)
+    model_configs = parse_field(configs, "model_config", ModelConfig)
+
+    parameters = Parameters(config_name)
+    several_param_names, s_params = get_several_config_params(
+        configs["finetuning"], parameters)
+
+    experiments = []
+    for model_i, model_config in enumerate(model_configs):
+        for dataset_i, dataset_config in enumerate(dataset_configs):
+            parameters.model_config = model_config 
+            parameters.dataset_config = dataset_config
+            os.makedirs(parameters.output_model_dataset_path, exist_ok=True)
+
+            config_dir_path = parameters.config_dir_path
+            with open(config_dir_path, 'w') as file:
+                yaml.dump(configs, file, default_flow_style=False)
+
+            for experiment_number, experiment_params in enumerate(s_params):
+                #print(f"Model {model_i} from {len(model_configs)}")
+                #print(f"Dataset {dataset_i} from {len(dataset_configs)}")
+                #print(f"Experiment {experiment_number} from {len(s_params)}")
+                #print(experiment_params)
+                #print(several_param_names)
+                assert len(experiment_params) == len(several_param_names)
+                cur_parameters = copy.deepcopy(parameters)
+                for param_i, param in enumerate(experiment_params):
+                    cur_parameters.__setattr__(several_param_names[param_i], param)
+                cur_parameters.experiment_number = experiment_number
+                #print("-" * 10, cur_parameters.__dict__, sep='\n')
+
+                os.makedirs(cur_parameters.output_experiment_path, exist_ok=True)
+                experiments.append(cur_parameters)
+    return experiments
--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 import argparse
-import copy
-import os
 import yaml
 
-from parameters import Parameters
 
 from single_function_executor import FineTuningExecutor
 from start_experiments import run_all_experiments
-from config_parsing import parse_field, get_several_config_params
-from config import DatasetConfig, ModelConfig
+
+from finetuning_parsing import create_finetuning_experiments
 
 def finetuning_main():
     parser = argparse.ArgumentParser()
@@ -19,43 +16,11 @@ def finetuning_main():
     with open(config_name, 'r') as file:
         configs = yaml.safe_load(file)
 
-
     parallel_config = {}
     if "parallel_config" in configs:
         parallel_config = configs.pop("parallel_config")
 
-    dataset_configs = parse_field(configs, "dataset_config", DatasetConfig)
-    model_configs = parse_field(configs, "model_config", ModelConfig)
-
-    parameters = Parameters(config_name)
-    several_param_names, s_params = get_several_config_params(configs["finetuning"], parameters)
-
-    experiments = []
-    for model_i, model_config in enumerate(model_configs):
-        for dataset_i, dataset_config in enumerate(dataset_configs):
-            parameters.model_config = model_config 
-            parameters.dataset_config = dataset_config
-            os.makedirs(parameters.output_model_dataset_path, exist_ok=True)
-
-            config_dir_path = parameters.config_dir_path
-            with open(config_dir_path, 'w') as file:
-                yaml.dump(configs, file, default_flow_style=False)
-
-            for experiment_number, experiment_params in enumerate(s_params):
-                #print(f"Model {model_i} from {len(model_configs)}")
-                #print(f"Dataset {dataset_i} from {len(dataset_configs)}")
-                #print(f"Experiment {experiment_number} from {len(s_params)}")
-                #print(experiment_params)
-                #print(several_param_names)
-                assert len(experiment_params) == len(several_param_names)
-                cur_parameters = copy.deepcopy(parameters)
-                for param_i, param in enumerate(experiment_params):
-                    cur_parameters.__setattr__(several_param_names[param_i], param)
-                cur_parameters.experiment_number = experiment_number
-                #print("-" * 10, cur_parameters.__dict__, sep='\n')
-
-                os.makedirs(cur_parameters.output_experiment_path, exist_ok=True)
-                experiments.append(cur_parameters)
+    experiments = create_finetuning_experiments(configs, config_name)
 
     print(f"Experiment amount: {len(experiments)}")
 

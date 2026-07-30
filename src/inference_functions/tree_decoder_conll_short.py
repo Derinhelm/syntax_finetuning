154f242bbab5074cc452f6a9e884a86a21a7cc26
derinhelm <derinhelm@yandex.ru>
2026-07-30 11:29:39 +0300

    Changing splitting in conll_short decoder

--- src/inference_functions/tree_decoder_conll_short.py
+++ src/inference_functions/tree_decoder_conll_short.py
@@ -1,11 +1,13 @@
+import re
+
 class TreeDecoderConllShort:
     def __init__(self, representation_type):
         self.representation_type = representation_type
 
     def decode_tree(self, answer_output, check_seq=False):
         try:
-            lines = answer_output.strip().split("\n").replace("\t", " ")
-            output = [line.strip().split(" ")
+            lines = answer_output.strip().split("\n")
+            output = [re.split(r'\s+', line.strip())
                 for line in lines]
             output = [line for line in output if len(output) == 4]
             res = [ { "id": line[0], "form": line[1],
